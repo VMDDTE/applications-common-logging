426f576f1c0fe0d61eb0c5fcdc96104c9753e942
Mo Gusbi <mo@mos-macbook-pro.home>
2021-01-21 10:31:41 +0000

    chore: fix rollup config to compile lib

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 import external from 'rollup-plugin-peer-deps-external';
 import pkg from './package.json';
 
@@ -24,13 +25,14 @@ export default [
       },
     ],
     plugins: [
+      json(),
       external(),
       babel({
         babelHelpers: 'bundled',
         extensions: ['.js', '.ts'],
       }),
       resolve({
-        extensions: ['.ts'],
+        extensions: ['.json', '.ts'],
       }),
       commonjs(),
     ],
